b675bebed6968093925317a5ca77cd8df37aa01b
LHcouto <xluisx_couto@hotmail.com>
2022-07-18 21:50:25 -0300

    feat:implement data

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,40 +1,48 @@
-import * as S from "./style";
-import { ReactComponent as Search } from "assets/icons/search.svg";
 import Menu from "components/Menu";
+import { DateTime } from "luxon";
+import { ReactComponent  as Search } from "assets/icons/search.svg";
+import * as S from "./style";
+import { RoutePath } from "types/routes";
+import { navigationItems } from "data/navigation";
 
 const Home = () => {
-  return (
-    <S.Home>
-      <Menu />
-      <S.HomeContent>
-        <header>
-          <S.HomeHeaderDetails>
-            <div>
-              <S.HomeHeaderDetailsLogo>Xbox Live</S.HomeHeaderDetailsLogo>
-              <S.HomeHeaderDetailsDate>
-                Aqui ficará a data
-              </S.HomeHeaderDetailsDate>
-            </div>
-            <S.HomeHeaderDetailsSearch>
-              <Search />
-              <input type="text" placeholder="Procure pelo Jogo" />
-            </S.HomeHeaderDetailsSearch>
-          </S.HomeHeaderDetails>
-        </header>
-        <div>
-          <S.HomeProductTitle>
-            <b>Jogos</b>
-          </S.HomeProductTitle>
-          <S.HomeProductList>
-            <p>Lista de Jogos aqui</p>
-          </S.HomeProductList>
-        </div>
-      </S.HomeContent>
-      <aside>
-        <p>Detalhes dos pedidos aqui</p>
-      </aside>
-    </S.Home>
-  );
+	const dateDescription = DateTime.now().toLocaleString({
+		...DateTime.DATE_SHORT,
+		weekday: "long",
+	});
+
+	return (
+		<S.Home>
+			<Menu active={RoutePath.HOME} navItems={navigationItems} />
+			<S.HomeContent>
+				<header>
+					<S.HomeHeaderDetails>
+						<div>
+							<S.HomeHeaderDetailsLogo>Xbox Live</S.HomeHeaderDetailsLogo>
+							<S.HomeHeaderDetailsDate>
+								{dateDescription}
+							</S.HomeHeaderDetailsDate>
+						</div>
+						<S.HomeHeaderDetailsSearch>
+							<Search />
+							<input type="text" placeholder="Procure pelo Jogo" />
+						</S.HomeHeaderDetailsSearch>
+					</S.HomeHeaderDetails>
+				</header>
+				<div>
+					<S.HomeProductTitle>
+						<b>Jogos</b>
+					</S.HomeProductTitle>
+					<S.HomeProductList>
+						<p>Lista de jogos aqui</p>
+					</S.HomeProductList>
+				</div>
+			</S.HomeContent>
+			<aside>
+				<p>Detalhes dos jogos aqui</p>
+			</aside>
+		</S.Home>
+	);
 };
 
 export default Home;
